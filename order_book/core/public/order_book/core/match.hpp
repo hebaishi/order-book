942e20d96d468effad775bc885dd55629f58b17e
Husam Hebaishi <hebaishi.husam@gmail.com>
2026-01-18 17:38:33 +0000

    Export order::Book and Match functions

--- order_book/core/public/order_book/core/match.hpp
+++ order_book/core/public/order_book/core/match.hpp
@@ -1,8 +1,9 @@
 #pragma once
+#include <core_export.h>
 #include <order_book/core/order/book.hpp>
 #include <order_book/core/trade.hpp>
 #include <vector>
 
 namespace order_book::core {
-std::vector<Trade> Match(order::Book &order_book);
+CORE_EXPORT std::vector<Trade> Match(order::Book &order_book);
 }
--- order_book/core/public/order_book/core/order/book.hpp
+++ order_book/core/public/order_book/core/order/book.hpp
@@ -1,10 +1,12 @@
 #pragma once
-#include <order_book/core/orders.hpp>
+#include <core_export.h>
 #include <order_book/core/order/symbol.hpp>
+#include <order_book/core/orders.hpp>
 namespace order_book::core::order {
 
-struct Book {
-  explicit Book(Symbol symbol);
+struct Book
+{
+  CORE_EXPORT explicit Book(Symbol symbol);
   BuyOrders buy_orders;
   SellOrders sell_orders;
   Symbol symbol{};
